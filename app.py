b54c320dde83246d973b25c4219752bd36b4bf00
tamtam-fitness <yu.tamura@raksul.com>
2023-08-28 23:53:56 +0900

    update

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-name = st.text_input('Your name')
-st.write("Hello,", name or "world")
+
+def main():
+    st.title("Matplotlib in Streamlit")
+
+    # データの生成
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+
+    # プロットの作成
+    fig, ax = plt.subplots()
+    ax.plot(x, y, label='sin(x)')
+    ax.set_title("A simple plot")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.legend()
+
+    # Streamlit にプロットを表示
+    st.pyplot(fig)
+
+
+if __name__ == "__main__":
+    main()
